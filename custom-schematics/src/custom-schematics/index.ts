48ea42fc04675fa8131394c16c113a49657dc333
anil verma <anilcs240@gmail.com>
2022-01-27 10:20:00 +0530

    finish basic angular sematics

--- custom-schematics/src/custom-schematics/index.ts
+++ custom-schematics/src/custom-schematics/index.ts
@@ -1,12 +1,30 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  mergeWith,
+  template,
+  url,
+} from "@angular-devkit/schematics";
+import {strings} from "@angular-devkit/core";
 import {IOptions} from "./options.interface";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function customSchematics(_options: IOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const {name} = _options;
-    tree.create("greet.js", `console.log('Hello ${name} , How r u!!')`);
-    return tree;
+    const templatUrl = url("./files");
+    const templateWithParameters = apply(templatUrl, [
+      template({..._options, ...strings}),
+    ]);
+    // const {componentName} = _options;
+    // tree.create(
+    //   "greet.js",
+    //   `console.log('Hello ${componentName} , How r u!!')`
+    // );
+    // return tree;
+
+    return mergeWith(templateWithParameters)(tree, _context);
   };
 }
--- custom-schematics/src/custom-schematics/options.interface.ts
+++ custom-schematics/src/custom-schematics/options.interface.ts
@@ -1,3 +1,3 @@
 export interface IOptions {
-  name: string;
+  componentName: string;
 }
